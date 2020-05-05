c95640eedcca9a5081c6ffce5c710d3608915381
movingfastball <movingfastballny2012@yahoo.co.jp>
2020-05-05 20:22:28 +0900

    初回コミット

---
+++ original2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>入力データ</title>
+</head>
+<body>
+    こんにちは、
+    <?=$_POST['name'] = ['山田' => '太郎' , 15 => '次郎','鈴木' => '三郎','四郎'];
+    ?>さん！
+</body>
+</html>
