7d7938ff37ac612cc9db13443b90d64208f62ed3
movingfastball <movingfastballny2012@yahoo.co.jp>
2020-05-24 23:23:40 +0900

    testコミット7

--- original.php
+++ original.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <form method="post" action = "original2.php">
-<label for = "name">名前3:</label>
+<label for = "name">名前7:</label>
 <!-- name属性をnameに設定しているからoriginal2.phpに情報を送れる -->
 <input id = "name" type = "text" name = "name" size = "15" />
 <input type = "submit" value = "送信">
