42ae8ac5203b7e2b301109c440e8bd1569e105f1
movingfastball <movingfastballny2012@yahoo.co.jp>
2020-07-08 00:27:51 +0900

    オライリー2‐2

---
+++ 2-2.php
@@ -0,0 +1,13 @@
+print <<<HTMLBLOCK
+<html>
+<head><tilte>Menu</title></head>
+<body ngcoler="#ffed9">
+    <h1>Dinner</h>
+<ul>
+    <li>Beef Chow-Fun
+    <li>Beef Chow-Fun2
+    <li>Beef Chow-Fun3
+</ul>
+<body>
+</html>
+HTMLBLOCK
